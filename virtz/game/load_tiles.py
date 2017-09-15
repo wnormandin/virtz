20da498810b82c96bea431be6507acace536fbfd
Bill Normandin (pokeybill) <bill@pokeybill.us>
2017-09-15 23:29:44 +0000

    Added game directory

---
+++ virtz/game/load_tiles.py
@@ -0,0 +1,40 @@
+import sys
+import pygame
+import pygame.locals
+
+SHEET_DIMS = (57, 31) # cols, rows
+SPRITE_MARGIN = 1
+SPRITE_DIMS = (16, 16) # width / height, in px
+SPRITE_SHEET_PATH = '/home/pokeybill/virtz/resources/basic_level.png'
+
+def load_tile_table(filename, width, height, margin=1):
+    image = pygame.image.load(filename).convert()
+    image_width, image_height = image.get_size() # 968px x 526px
+    tile_table = []
+    for tile_x in range(SHEET_DIMS[0]):
+        line = []
+        tile_table.append(line)
+        for tile_y in range(SHEET_DIMS[1]):
+            x_loc = tile_x * (width + margin)
+            y_loc = tile_y * (height + margin)
+            rect = (x_loc, y_loc, width, height)
+            line.append(image.subsurface(rect))
+    return tile_table
+
+if __name__ == '__main__':
+    # When invoked as a script, display the passed tilemap
+    # tiles split and slightly separated
+    pygame.init()
+    screen = pygame.display.set_mode((913, 496))
+    screen.fill((255, 255, 255))
+    table = load_tile_table(SPRITE_SHEET_PATH, *SPRITE_DIMS)
+    for x, row in enumerate(table):
+        for y, tile in enumerate(row):
+            x_loc = x * SPRITE_DIMS[0]
+            y_loc = y * SPRITE_DIMS[1]
+            screen.blit(tile, (x_loc, y_loc))
+    pygame.display.flip()
+
+    # Wait for input after displaying the tile map
+    while pygame.event.wait().type != pygame.locals.QUIT:
+        pass
